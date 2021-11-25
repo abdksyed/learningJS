589ddd28f8732b9b87f2bd9c4cc9041b7ebc0d46
Syed Abdul <abdksyed@gmail.com>
2021-11-25 17:30:53 +0530

    Initial Commit

---
+++ 00_Hello/hello.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+// > Hello World!
+
+console.log(fullName); //Hoisting
+// > undefined
+
+var fullName = 'ThisIsMyName';
+console.log(fullName);
+// > ThisIsMyName
+
+var amICorrect = true;
+console.log("Am I Correct?", amICorrect);
+// > Am I Correct?  true
+
+var myNumber = 47;
+console.log(myNumber);
+// > 47
